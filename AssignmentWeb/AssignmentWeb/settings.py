2da22d2e290ab39b22655231d69d1fc096a13c18
arifdel <mohd.arif.siddiqui@accenture.com>
2020-08-11 02:32:29 +0530

    remove secret key

--- AssignmentWeb/AssignmentWeb/settings.py
+++ AssignmentWeb/AssignmentWeb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@((rt9epkyrncjhu#5jm6#*5*%(l%9l9j*968bjz5kgax-1m8d'
+SECRET_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
